87e47d91c7296c19b35cc1a8764e1bd3c28382db
Dylan Harrison <dillysnaps@gmail.com>
2018-12-03 15:03:57 -0600

    client mapeditor setup

--- client/src/app/components/mapeditor/mapeditor.component.ts
+++ client/src/app/components/mapeditor/mapeditor.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IsoCanvas } from '../../engine/isocanvas';
 
 @Component({
   selector: 'app-mapeditor',
@@ -7,9 +8,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapeditorComponent implements OnInit {
 
+  myCanvas: IsoCanvas;
+  layoutVertical: boolean; // 0:horizontal, 1:vertical
+
   constructor() { }
 
   ngOnInit() {
+
+    this.layoutVertical = window.innerHeight > window.innerWidth;
+    if (this.layoutVertical) {
+      document.getElementById('vcontainer').style.bottom = '200px';
+      document.getElementById('toolpanel1').style.width = '0px';
+      document.getElementById('isocanvas').style.right = '0px';
+      document.getElementById('toolpanel2').style.height = '200px';
+    } else {
+      document.getElementById('vcontainer').style.bottom = '0px';
+      document.getElementById('toolpanel1').style.width = '300px';
+      document.getElementById('isocanvas').style.right = '300px';
+      document.getElementById('toolpanel2').style.height = '0px';
+    }
+    
+    this.myCanvas = new IsoCanvas(<HTMLDivElement>document.getElementById('isocanvas'));
+    window.addEventListener('resize', (ev) => {
+      
+      if (window.innerHeight > window.innerWidth != this.layoutVertical) {
+        this.layoutVertical = window.innerHeight > window.innerWidth;
+        if (this.layoutVertical) {
+          document.getElementById('vcontainer').style.bottom = '200px';
+          document.getElementById('toolpanel1').style.width = '0px';
+          document.getElementById('isocanvas').style.right = '0px';
+          document.getElementById('toolpanel2').style.height = '200px';
+        } else {
+          document.getElementById('vcontainer').style.bottom = '0px';
+          document.getElementById('toolpanel1').style.width = '300px';
+          document.getElementById('isocanvas').style.right = '300px';
+          document.getElementById('toolpanel2').style.height = '0px';
+        }
+      }
+      this.myCanvas.paint();
+    });
+    document.getElementById('isocanvas').addEventListener('click', (ev) => {                                
+      this.myCanvas.defaultMouseClickListener(ev);
+      this.myCanvas.paint();
+    });
+    document.getElementById('isocanvas').addEventListener('wheel', (ev) => {
+      this.myCanvas.defaultMouseWheelListener(ev);
+      this.myCanvas.paint();
+    });
+    document.onkeypress = (event) => {
+        if (event.key == 'r') {
+          //this.myCanvas.setRotation(this.myCanvas._isoRotation + 1);
+        }
+    };
+    this.myCanvas.paint();
+    
   }
 
 }
