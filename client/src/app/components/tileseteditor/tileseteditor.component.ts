154c1ab94eeadecce7fbb76ce9b59fffbc3ca6cb
Dylan Harrison <dillysnaps@gmail.com>
2018-12-05 02:34:09 -0600

    client tileseteditor properties accordian

--- client/src/app/components/tileseteditor/tileseteditor.component.ts
+++ client/src/app/components/tileseteditor/tileseteditor.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { IsoCanvas } from '../../engine/isocanvas';
+import { IsoTile } from '../../engine/isotile';
+import { IsoTileSet } from '../../engine/isotileset';
 
 @Component({
   selector: 'app-tileseteditor',
@@ -7,9 +10,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TileseteditorComponent implements OnInit {
 
+  
+  layoutVertical: boolean; // 0:horizontal, 1:vertical
+  cursor = {
+    'cursor': false,
+    'width': 128,
+    'height': 128
+  }
+  tileset = null;
+  selectedTile = null;
+
   constructor() { }
 
   ngOnInit() {
+
+    this.layoutVertical = window.innerHeight > window.innerWidth;
+    if (this.layoutVertical) {
+      document.getElementById('vcontainer').style.bottom = '200px';
+      document.getElementById('toolpanel1').style.width = '0px';
+      document.getElementById('isocanvas').style.right = '0px';
+      document.getElementById('toolpanel2').style.height = '200px';
+    } else {
+      document.getElementById('vcontainer').style.bottom = '0px';
+      document.getElementById('toolpanel1').style.width = '300px';
+      document.getElementById('isocanvas').style.right = '300px';
+      document.getElementById('toolpanel2').style.height = '0px';
+    }
+
+    window.addEventListener('resize', (ev) => {
+      if (window.innerHeight > window.innerWidth != this.layoutVertical) {
+        this.layoutVertical = window.innerHeight > window.innerWidth;
+        if (this.layoutVertical) {
+          document.getElementById('vcontainer').style.bottom = '200px';
+          document.getElementById('toolpanel1').style.width = '0px';
+          document.getElementById('isocanvas').style.right = '0px';
+          document.getElementById('toolpanel2').style.height = '200px';
+        } else {
+          document.getElementById('vcontainer').style.bottom = '0px';
+          document.getElementById('toolpanel1').style.width = '300px';
+          document.getElementById('isocanvas').style.right = '300px';
+          document.getElementById('toolpanel2').style.height = '0px';
+        }
+      }
+    });
+    this.tileset = new IsoTileSet();
   }
 
+  loadtileset() {
+    this.tileset.dumbLoad(() => {
+
+    });
+  }
+
+  templating = {
+    isBoolean: (x: any) => {
+      return typeof x === "boolean";
+    },
+    isNumber: (x: any) => {
+      return typeof x === "number";
+    },
+    isString: (x: any) => {
+      return !(this.templating.isBoolean(x) ||this.templating.isNumber(x));
+    }
+  }
 }
