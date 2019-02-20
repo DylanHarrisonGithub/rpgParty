b1a01cf0cec1cc9f63f16547ee6f1c2f101d9ead
DylanHarrisonGithub <dillysnaps@gmail.com>
2019-02-19 23:52:30 -0600

    server get tileset and map file list

--- routes/assetrouter.js
+++ routes/assetrouter.js
@@ -1,10 +1,32 @@
+const fs = require('fs')
 let path = require('path');
 
 module.exports = (router) => {
+
+    router.get('/tilesets', (req, res) => {
+        fs.readdir('./public/assets/tilesets/', (err, tilesetlist) => {
+            if (err) {
+                res.json({ message: 'error getting tileset list', 'error': err});
+            } else {
+                res.json({ tilesets: tilesetlist });
+            }
+        });
+    });
+
     router.get('/tilesets/:file', (req, res) => {
         res.sendFile('/public/assets/tilesets/' + req.params.file, {'root': './'}, (error) => {
             if (error) {
                 res.json({'message': 'could not find tileset ' + req.params.file, 'error': error });
+            }
+        });
+    });
+
+    router.get('/maps', (req, res) => {
+        fs.readdir('./public/assets/maps/', (err, maplist) => {
+            if (err) {
+                res.json({ message: 'error getting maps list', 'error': err});
+            } else {
+                res.json({ maps: maplist });
             }
         });
     });
