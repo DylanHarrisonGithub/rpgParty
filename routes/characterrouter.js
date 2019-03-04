b87148bc0957e3b54010a2938671d55ce718b7f0
DylanHarrisonGithub <dillysnaps@gmail.com>
2019-03-04 14:32:16 -0600

    server character update and delete

--- routes/characterrouter.js
+++ routes/characterrouter.js
@@ -69,5 +69,57 @@ module.exports = (router => {
     }
   });
 
+  router.delete('/', (req, res) => {
+    if (req.headers.authorization) {
+      jsonwebtoken.verify(req.headers.authorization, config[env].JWT_SECRET, (err, decoded) => {
+        if (err) {
+          res.json({ success: false, message: "Authentication was not valid.", err: err });
+        } else {
+          if (req.body._id) {
+            Character.deleteOne({ _id: req.body._id, owner_id: decoded._id }, (err) => {
+              if (err) {
+                res.json({ success: false, message: "Error deleting character.", error: err});
+              } else {
+                res.json({ success: true, message: "Character deleted."})
+              }
+            });
+          } else {
+            res.json({ success: false, message: "Could not delete because character id was not provided."});
+          }
+        }
+      });
+    } else {
+      res.json({ success: false, message: "Must provide authentication for protected route."});
+    }         
+  });
+
+  router.put('/', (req, res) => {
+    if (req.headers.authorization) {
+      jsonwebtoken.verify(req.headers.authorization, config[env].JWT_SECRET, (err, decoded) => {
+        if (err) {
+          res.json({ success: false, message: "Authentication was not valid.", err: err });
+        } else {
+          if (req.body._id) {
+            if (req.body.character) {
+              Character.updateOne({ _id: req.body._id }, req.body.character, (err, doc) => {
+                if (err) {
+                  res.json({ success: false, message: "Error updating character.", error: err });
+                } else {
+                  res.json({ success: true, message: "Character successfully updated.", doc: doc });
+                }
+              });
+            } else {
+              res.json({ success: false, message: "Error updating. Character was not provided." });
+            }
+          } else {
+            res.json({ success: false, message: "Could not update because character id was not provided."});
+          }
+        }
+      });
+    } else {
+      res.json({ success: false, message: "Must provide authentication for protected route."});
+    } 
+  });
+
   return router;
 });
